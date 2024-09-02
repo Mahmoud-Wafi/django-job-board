4858851404c577bcf74cf8521baa79f05317921f
Mahmoud-Wafi <mahmoudwafi33@gmail.com>
2024-09-02 21:44:59 +0300

    add views to job app

--- job/admin.py
+++ job/admin.py
@@ -4,3 +4,7 @@ from .models import Job , Category
 # Register your models here.
 admin.site.register(Job)
 admin.site.register(Category)
+
+
+admin.site.site_header='Job Board'
+admin.site.site_title='Job Board'
